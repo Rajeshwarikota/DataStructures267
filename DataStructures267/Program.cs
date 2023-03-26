7ce73f0b418d9fdc50a07c790db9bfee9dddab18
Rajeshwarikota <kotarajeswari003.com>
2023-03-26 15:47:06 +0530

    [Rajeshwari kota] Adding sorting elements in descending order

--- DataStructures267/Program.cs
+++ DataStructures267/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructures267
         static void Main(string[] args)
         {
             Console.WriteLine("....Data structures....");
-            Console.WriteLine("\n1:Custom Linked list\n2:Custom stack\n3:custom ques\n4:Built-in linked list\n5:Built-In Stack\n6:Built-In Queue\n22:Default");
+            Console.WriteLine("\n1:Custom Linked list\n2:Custom stack\n3:custom ques\n4:Built-in linked list\n5:Built-In Stack\n6:Built-In Queue\n7:Sorting in Descending\n22:Default");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -112,6 +112,9 @@ namespace DataStructures267
                     break;
                 case 6:
                     BuiltInQue.CRUDOperation();
+                    break;
+                case 7:
+                    SortingInDescending.Descending();
                     break;
             }
             Console.ReadLine();
---
+++ DataStructures267/SortingInDescending.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures267
+{
+    internal class SortingInDescending
+    {
+        public static void Descending()
+        {
+            // Create a new LinkedList
+            LinkedList<int> myList = new LinkedList<int>();
+
+            // Add some values to the list
+            myList.AddLast(45);
+            myList.AddLast(37);
+            myList.AddLast(12);
+
+            // Create a new node to hold the value to insert
+            LinkedListNode<int> newNode = new LinkedListNode<int>(40);
+
+            // Traverse the list until you find the correct position to insert the new node
+            LinkedListNode<int> currentNode = myList.First;
+            while (currentNode != null && currentNode.Value > newNode.Value)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            // Insert the new node at the correct position
+            if (currentNode == null)
+            {
+                myList.AddLast(newNode);
+            }
+            else
+            {
+                myList.AddBefore(currentNode, newNode);
+            }
+
+            // Print the updated list
+            Console.WriteLine("Elements in the list (in descending order):");
+            foreach (int element in myList)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
